e65a710bb87d251b3599b1a0dc5fe7acaaf0f298
elijahsnoz <elijahsnoz@gmail.com>
2024-03-11 13:15:24 -0700

    9-easter_egg

--- 0x00-python-hello_world/9-easter_egg.py
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -1,21 +1,2 @@
 #!/usr/bin/python3
-print(f"The Zen of Python, by Tim Peters\n"
-      f"\nBeautiful is better than ugly.\n"
-      f"Explicit is better than implicit.\n"
-      f"Simple is better than complex.\n"
-      f"Complex is better than complicated.\n"
-      f"Flat is better than nested.\n"
-      f"Sparse is better than dense.\n"
-      f"Readability counts.\n"
-      f"Special cases aren't special enough to break the rules.\n"
-      f"Although practicality beats purity.\n"
-      f"Errors should never pass silently.\n"
-      f"Unless explicitly silenced.\n"
-      f"In the face of ambiguity, refuse the temptation to guess.\n"
-      f"There should be one-- and preferably only one --obvious way to do it.\n"
-      f"Although that way may not be obvious at first unless you're Dutch.\n"
-      f"Now is better than never.\n"
-      f"Although never is often better than *right* now.\n"
-      f"If the implementation is hard to explain, it's a bad idea.\n"
-      f"If the implementation is easy to explain, it may be a good idea.\n"
-      f"Namespaces are one honking great idea -- let's do more of those!")
+import this
