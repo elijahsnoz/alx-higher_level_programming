cfff15672ec0e9cf220000c77f5117c1356fcfbf
elijahsnoz <elijahsnoz@gmail.com>
2024-03-13 03:29:24 +0100

    10-add

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def add(a, b):
+    """
+    Adds two integers and returns the result.
+
+    Args:
+        a: An integer.
+        b: An integer.
+
+    Returns:
+        The result of adding a and b.
+    """
+    # Using the '+' operator to add two integers
+    return a + b
