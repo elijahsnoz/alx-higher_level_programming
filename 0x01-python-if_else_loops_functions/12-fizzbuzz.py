846c98212408d42af687d85a3a1086edddb47760
elijahsnoz <elijahsnoz@gmail.com>
2024-03-13 16:01:36 +0100

    12-fizzbuzz.py

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    """
-    Prints numbers from 1 to 100 with replacements for multiples of three, five, and both.
-
-    Returns:
-        None
-    """
     for i in range(1, 101):
         if i % 3 == 0 and i % 5 == 0:
             print("FizzBuzz", end=" ")
@@ -16,6 +10,5 @@ def fizzbuzz():
         else:
             print(i, end=" ")
 
-
-# Call the function to print the FizzBuzz sequence
+# Call the function to test
 fizzbuzz()
