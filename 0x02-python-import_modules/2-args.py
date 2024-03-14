7e727e009e86471128587d41e016b3ec46f428fe
elijahsnoz <elijahsnoz@gmail.com>
2024-03-15 00:26:11 +0100

    no of arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+
+    count = len(sys.argv) - 1
+
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("Number of argument(s): 1.")
+        print("1: {}.".format(sys.argv[1]))
+    else:
+        print("Number of argument(s): {}.".format(count))
+        for i in range(1, count + 1):
+            print("{}: {}.".format(i, sys.argv[i]))
+
