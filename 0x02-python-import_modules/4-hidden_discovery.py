09345bbaae0893ec9d4b5b63ba1453296bc9e1bf
elijahsnoz <elijahsnoz@gmail.com>
2024-03-15 02:07:34 +0100

    print all funtion

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import hidden_4
+
+if __name__ == "__main__":
+    names = [name for name in dir(hidden_4) if not name.startswith("__")]
+    names.sort()
+    for name in names:
+        print(name)
+
