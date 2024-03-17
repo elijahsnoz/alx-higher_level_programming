cac8ac597fe0335152634d50dfdf2bf70abff65e
elijahsnoz <elijahsnoz@gmail.com>
2024-03-17 23:17:20 +0100

    Lists of lists

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if not matrix:
+        print("Empty matrix")
+        return
     for row in matrix:
+        if not row:
+            print("Empty row")
+            continue
         for num in row:
             print("{:d}".format(num), end=" ")
         print()
