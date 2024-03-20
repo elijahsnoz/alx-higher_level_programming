6e4979ca7d4f5062d30b011acbc48c21d27584bf
elijahsnoz <elijahsnoz@gmail.com>
2024-03-20 16:58:21 +0100

    1-search_replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = []
-    for item in my_list:
-        # If the element matches the 'search' element, replace it with the 'replace' element
-        if item == search:
-            new_list.append(replace)
-        else:
-            new_list.append(item)
-    return new_list
+    '''
+        A function that traverse through a list for an element that matches
+        search and modify it with replace then returns a new list.
+
+        @elem: Elements
+    '''
+    if len(my_list) == 0:
+        return my_list
+
+    new_lst = [elem if elem != search else replace for elem in my_list]
+    return new_lst
