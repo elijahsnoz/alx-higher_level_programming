feb9578ce3558a20ac5bfe311763df6047208184
elijahsnoz <elijahsnoz@gmail.com>
2024-03-20 16:47:39 +0100

    1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    """
-    Parameters:
-    my_list (list): The initial list.
-    """
-    # Create a new list to store the modified elements
     new_list = []
     for item in my_list:
         # If the element matches the 'search' element, replace it with the 'replace' element
