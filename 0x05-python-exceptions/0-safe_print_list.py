9bb5c9f06b7ce7fa88c1d1433a9e67d681251ce3
elijahsnoz <elijahsnoz@gmail.com>
2024-04-02 12:58:46 +0100

    0-safe

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,4 +1,4 @@
-#!/usr/bin.python3
+#!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     printed = 0
     try:
@@ -20,4 +20,3 @@ if __name__ == "__main__":
     print("nb_print: {:d}".format(nb_print))
     nb_print = safe_print_list(my_list, len(my_list) + 2)
     print("nb_print: {:d}".format(nb_print))
-
