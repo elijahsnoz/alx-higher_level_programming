75e423a5f4f58377ebe295cf5a9ee7651c406d65
elijahsnoz <elijahsnoz@gmail.com>
2024-04-03 08:54:53 +0100

    magicwork

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -9,7 +9,7 @@ class MagicClass:
         self.__radius = radius
 
     def area(self):
-        return 2 * math.pi * self.__radius ** 2
+        return 2 * math.pi * (self.__radius ** 2)
 
     def circumference(self):
         return 2 * math.pi * self.__radius
