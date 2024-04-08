e44087e20b148c512a0a8cd54341fb6184d9d6f1
elijahsnoz <elijahsnoz@gmail.com>
2024-04-08 22:19:43 +0100

    2 rectangle.py

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
+"""
+This is the "Rectangle"  module.
+
+This module provides a simple Rectangle class.
+"""
+
+
 class Rectangle:
+    """A Rectangle class with attributes width and height, and
+    methods area and perimiter.
+    """
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
@@ -10,12 +20,11 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("width must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
-        else:
-            self.__width = value
+        self.__width = value
 
     @property
     def height(self):
@@ -23,17 +32,16 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("height must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
-        else:
-            self.__height = value
+        self.__height = value
 
     def area(self):
-        return self.width * self.height
+        return self.__width * self.__height
 
     def perimeter(self):
-        if self.width == 0 or self.height == 0:
+        if self.__width is 0 or self.__height is 0:
             return 0
-        return 2 * (self.width + self.height)
+        return (2 * self.__width) + (2 * self.__height)
