c789d49853e079c1f9c55b02df2151362a173231
elijahsnoz <elijahsnoz@gmail.com>
2024-04-08 21:57:29 +0100

    1rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,42 +1,37 @@
+#!/usr/bin/python3
+"""
+This is the "Rectangle"  module.
+
+This module provides a simple Rectangle class with attribute width and height.
+Default values of both attributes are 0.
+"""
+
 class Rectangle:
+    """A Rectangle class with attributes  width and height"""
     def __init__(self, width=0, height=0):
-        # Initialize private instance attributes
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
-        # Getter method for retrieving width
         return self.__width
 
     @width.setter
     def width(self, value):
-        # Setter method for setting width
-        if not isinstance(value, int):
-            # Raise TypeError if width is not an integer
-            raise TypeError("width must be an integer")
-        elif value < 0:
-            # Raise ValueError if width is negative
-            raise ValueError("width must be >= 0")
-        else:
-            # Set width if it meets conditions
-            self.__width = value
+        if type(value) is not int:
+            raise TypeError('width must be an integer')
+        if value < 0:
+            raise ValueError('width must be >= 0')
+        self.__width = value
 
     @property
     def height(self):
-        # Getter method for retrieving height
         return self.__height
 
     @height.setter
     def height(self, value):
-        # Setter method for setting height
-        if not isinstance(value, int):
-            # Raise TypeError if height is not an integer
-            raise TypeError("height must be an integer")
-        elif value < 0:
-            # Raise ValueError if height is negative
-            raise ValueError("height must be >= 0")
-        else:
-            # Set height if it meets conditions
-            self.__height = value
-
+        if type(value) is not int:
+            raise TypeError('height must be an integer')
+        if value < 0:
+            raise ValueError('height must be >= 0')
+        self.__height = value
