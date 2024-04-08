7475f0901e1139a3ae071635f2f2356fcd47878b
elijahsnoz <elijahsnoz@gmail.com>
2024-04-08 22:35:28 +0100

    3-rectangle.py

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
+"""
+This is the "Rectangle"  module.
+
+This module provides a simple Rectangle class.
+"""
+
+
 class Rectangle:
     """A Rectangle class with attributes width and height, and
-    methods area, perimeter, and str.
+    methods area, perimeter, print, and str.
     """
     def __init__(self, width=0, height=0):
         self.width = width
@@ -13,7 +20,7 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -25,21 +32,26 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
 
     def __str__(self):
+        total = ""
         if self.__height == 0 or self.width == 0:
-            return ""
-        return '\n'.join(['#' * self.__width for _ in range(self.__height)])
+            return total
+        for i in range(self.__height):
+            total += ("#" * self.__width)
+            if i is not self.__height - 1:
+                total += "\n"
+        return total
 
     def area(self):
         return self.__width * self.__height
 
     def perimeter(self):
-        if self.__width == 0 or self.__height == 0:
+        if self.__width is 0 or self.__height is 0:
             return 0
-        return 2 * (self.__width + self.__height)
+        return (2 * self.__width) + (2 * self.__height)
