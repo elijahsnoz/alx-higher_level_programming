c123692831e7a8d8406e8e7bfb8f810acac214af
elijahsnoz <elijahsnoz@gmail.com>
2024-04-24 22:58:48 +0100

    Display

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -77,8 +77,10 @@ class Rectangle(Base):
 
     def display(self):
         """ Displays the rectangle with '#' character """
+        for _ in range(self.y):
+            print()
         for _ in range(self.height):
-            print("#" * self.width)
+            print(" " * self.x + "#" * self.width)
 
     def __str__(self):
         """ Returns string representation of the rectangle """
