fdc09421f83fb9cd22cf37eea83f169bfaa34ab4
elijahsnoz <elijahsnoz@gmail.com>
2024-05-22 03:08:35 +0100

    first table

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,11 +1,5 @@
--- the database hbtn_0c_0 if it doesn't already exist.
-CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
-
--- Switch to the database hbtn_0c_0
-USE hbtn_0c_0;
-
--- This script creates the table first_table in the current database if it doesn't already exist.
+-- Create the table only if it does not already exist
 CREATE TABLE IF NOT EXISTS first_table (
-    id INT,
-    name VARCHAR(256)
+    id INT,          -- Define id column as an integer
+    name VARCHAR(256) -- Define name column as a variable-length string with a maximum length of 256 characters
 );
