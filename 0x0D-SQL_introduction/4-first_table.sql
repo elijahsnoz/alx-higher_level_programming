5a5e1b94f4f776b65e40e04712cc1e94779a6ae8
elijahsnoz <elijahsnoz@gmail.com>
2024-05-22 02:17:04 +0100

    First table

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,4 +1,4 @@
--- This script creates the database hbtn_0c_0 if it doesn't already exist.
+-- the database hbtn_0c_0 if it doesn't already exist.
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
 
 -- Switch to the database hbtn_0c_0
