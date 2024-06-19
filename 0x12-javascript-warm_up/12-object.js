5184640e85ba14e1d2ac6ffc29cd017c9fec4407
elijahsnoz <elijahsnoz@gmail.com>
2024-06-19 21:56:10 +0100

    12-object.js

--- 0x12-javascript-warm_up/12-object.js
+++ 0x12-javascript-warm_up/12-object.js
@@ -1,9 +1,12 @@
 #!/usr/bin/node
 const myObject = {
-    type: 'object',
-    value: 12
-  };
-  console.log(myObject);
-  myObject.value = 89;
-  console.log(myObject);
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+
+/* YOUR CODE HERE */
+myObject.value = 89;
+
+console.log(myObject);
 
