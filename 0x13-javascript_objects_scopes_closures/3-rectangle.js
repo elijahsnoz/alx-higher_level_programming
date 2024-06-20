13f582c2dcea35ad6903b119446e83fda8a35ea0
elijahsnoz <elijahsnoz@gmail.com>
2024-06-20 08:49:22 +0100

    3-rect

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
-    constructor (w, h) {
-      if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
-    }
+  constructor (w, h) {
+    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
+  }
 
-    print () {
-      for (let i = 0; i < this.height; i++) console.log('X'.repeat(this.width));
-    }
-  };
+  print () {
+    for (let i = 0; i < this.height; i++) console.log('X'.repeat(this.width));
+  }
+};
