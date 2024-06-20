e9da11899a53e892ed1859fbee21d8864dd0bce1
elijahsnoz <elijahsnoz@gmail.com>
2024-06-20 09:32:30 +0100

    work

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,25 +1,11 @@
 #!/usr/bin/node
-
-// Import the dictionary from the required file
 const dict = require('./101-data.js').dict;
-
-// Initialize an empty dictionary for the new structure
 let newDict = {};
-
-// Iterate over each key in the imported dictionary
 for (let key in dict) {
-  // Get the occurrence value
-  let value = dict[key];
-  
-  // Check if the value exists in the new dictionary
-  if (!newDict[value]) {
-    // If not, create an array with the current key
-    newDict[value] = [key];
+  if (newDict[dict[key]] === undefined) {
+    newDict[dict[key]] = [key];
   } else {
-    // If it exists, push the current key into the array
-    newDict[value].push(key);
+    newDict[dict[key]].push(key);
   }
 }
-
-// Print the new dictionary
 console.log(newDict);
