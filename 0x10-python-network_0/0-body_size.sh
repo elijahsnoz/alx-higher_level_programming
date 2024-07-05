35f3af8ecd495485e3bff90d31dd0350190f01a9
elijahsnoz <elijahsnoz@gmail.com>
2024-07-05 20:37:00 +0100

    hbtn_status

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
-# Script that takes in a URL, sends a request to that URL, and displays the size of the body of the response in bytes.
-
-# Check if a URL was provided
-if [ -z "$1" ]; then
-  echo "Usage: $0 <URL>"
-  exit 1
-fi
-
-# Use curl to get the size of the response body
-curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
-
+# ends a request to that URL displays the size of the response body
+curl -sI "$1" | grep -i Content-Length | cut -d " " -f2
