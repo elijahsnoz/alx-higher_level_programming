d3d45d8506d5c3ffb53415ddddc6bfad189d4812
elijahsnoz <elijahsnoz@gmail.com>
2024-07-08 08:20:37 -0700

    body

---
+++ 0x10-python-network_0/1-body.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Send a GET request to a URL and display the body of the response if status code is 200
+http_code=$(curl -sL -w "%{http_code}" -o /dev/null "$1")
+if [ "$http_code" == '200' ]; then
+    curl -sL "$1"
+fi
