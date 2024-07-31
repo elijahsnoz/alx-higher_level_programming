360f32e7c33920ca306af03e96878cc7ceb36eae
elijahsnoz <elijahsnoz@gmail.com>
2024-08-01 00:52:17 +0100

    request_store

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -3,29 +3,10 @@
 const request = require('request');
 const fs = require('fs');
 
-// Check if the correct number of arguments is provided
-if (process.argv.length < 4) {
-  console.error('Usage: ./5-request_store.js <URL> <file_path>');
-  process.exit(1);
-}
-
-// Get the URL and file path from the command-line arguments
-const url = process.argv[2];
-const filePath = process.argv[3];
-
-// Make the GET request to the URL
-request(url, function (err, res, body) {
-  if (err) {
-    console.error('Error fetching URL:', err);
-    process.exit(1);
-  }
-
-  // Write the response body to the specified file in UTF-8 encoding
-  fs.writeFile(filePath, body, 'utf8', function (writeErr) {
-    if (writeErr) {
-      console.error('Error writing file:', writeErr);
-      process.exit(1);
+request(process.argv[2], function (_err, _res, body) {
+  fs.writeFile(process.argv[3], body, 'utf8', function (err) {
+    if (err) {
+      console.log(err);
     }
-    console.log('File saved successfully');
   });
 });
